3ab0fce30037ea272dd5c527f976b9b0836cb623
unknown <ykurabuchi@ogic2.local>
2020-11-26 16:50:35 +0900

    new is ok

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,22 @@
+class CompaniesController < ApplicationController
+  def index
+  end
+
+  def new
+    @companies = Company.new
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  
+end
